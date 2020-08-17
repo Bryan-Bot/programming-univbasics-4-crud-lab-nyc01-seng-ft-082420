d5f5282686c1229bdc6d96a93f76963342b1b020
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-08-17 13:44:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,11 +27,11 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-
+  array.unshift
 end
 
 def retrieve_last_element_from_array(array)
-
+  array.push
 end
 
 def update_element_from_index(array, index_number, element)
